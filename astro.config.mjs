a7f29e23d1f24e2d35203113ff8f173ff1bff44e
Lucas <lucas.ribeiro711@gmail.com>
2022-11-24 11:59:57 -0300

    chore: add and configure robotstxt and gtag

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,14 @@
 import { defineConfig } from 'astro/config';
-
-// https://astro.build/config
+import partytown from "@astrojs/partytown";
+import robotsTxt from 'astro-robots-txt';
 import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()]
+  integrations: [partytown({
+    // Adds dataLayer.push as a forwarding-event.
+    config: {
+      forward: ["dataLayer.push"],
+    }
+  }), robotsTxt(), tailwind()]
 });
