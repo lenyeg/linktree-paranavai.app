54336090edd0e7697bc84aabedb037a95e749073
André Vandal <me@andrevandal.dev>
2022-11-25 15:54:48 -0300

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,8 +5,10 @@ import tailwind from "@astrojs/tailwind";
 // https://astro.build/config
 import sitemap from "@astrojs/sitemap";
 
+const { CF_PAGES_URL } = process.env || {}
+
 // https://astro.build/config
 export default defineConfig({
-  site: 'https:pvai.app',
+  site: CF_PAGES_URL || 'https://pvai.app',
   integrations: [robotsTxt(), tailwind(), sitemap()]
 });
