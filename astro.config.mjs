f201a06124e8825ee1ff22926e52084d8c2eb95f
Lucas <lucas.ribeiro711@gmail.com>
2022-11-25 15:22:55 -0300

    test: sitemap config

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,5 +7,6 @@ import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https:pvai.app',
   integrations: [robotsTxt(), tailwind(), sitemap()]
 });
