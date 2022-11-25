96448e65050488c0aba214a59a4ac82704a91129
André Vandal <1340508+andrevandal@users.noreply.github.com>
2022-11-25 11:20:59 -0300

    chore: remove partytown

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,8 @@
 import { defineConfig } from 'astro/config';
-import partytown from "@astrojs/partytown";
 import robotsTxt from 'astro-robots-txt';
 import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [partytown({
-    // Adds dataLayer.push as a forwarding-event.
-    config: {
-      forward: ["dataLayer.push"],
-    }
-  }), robotsTxt(), tailwind()]
+  integrations: [robotsTxt(), tailwind()]
 });
